79f1e18e8c2b3a2902188babc1e0bde05c922044
Simon Cordova <>
2021-06-06 13:19:35 +0100

    Create slider functionality, use mock color data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,97 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+
+import { randomColors } from './data';
 
 function App() {
+  const [colors] = useState(randomColors);
+  const [index, setIndex] = useState(0);
+  const [paused, setPaused] = useState(true);
+
+  useEffect(() => {
+    const lastIndex = colors.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, colors]);
+
+  const handlePreviousButton = () => {
+    setIndex(index - 1);
+    setPaused(true);
+  }
+  
+  const handleNextButton = () => {
+    setIndex(index + 1);
+    setPaused(true);
+  }
+
+  useEffect(() => {
+    let slider;
+    if (!paused) {
+      slider = setInterval(() => {
+        setIndex(index + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(slider);
+    }
+  }, [paused, index])
   return (
     <div className="App">
-      Hello World
+      <section className="section">
+        <div className="section__title">
+          <h2>
+            Colours
+          </h2>
+        </div>
+          <div className="section__body">
+            {randomColors.map((colours, personIndex) => {
+              const { name } = colours;
+              let position = 'nextSlide';
+              if (personIndex === index) {
+                position = 'activeSlide';
+              }
+              if (
+                personIndex === index - 1 ||
+                (index === 0 && personIndex === colours.length - 1)
+              ) {
+                position = 'lastSlide';
+              }
+              return (
+                <article className={`${position} article`} key={name}>
+                  <div
+                    className="slide-background-color"
+                    style={{ backgroundColor: `${name}` }}
+                    />
+                  <div className="article__controls">
+                    <h4>{name}</h4>
+                  </div>
+                </article>
+              );
+            })}
+          </div>
+        <div className="section__footer">
+          <button
+            className="button section__body__button"
+            onClick={() => handlePreviousButton()}>
+            <FiChevronLeft />
+          </button>
+          <button
+            className="button section__body__button"
+            onClick={() => setPaused(!paused)}>
+              { paused ? <BsFillPlayFill /> : <BsFillPauseFill /> }
+          </button>
+          <button
+            className="button section__body__button"
+            onClick={() => handleNextButton()}>
+            <FiChevronRight />
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,443 @@
+export const randomColors = [
+  {
+    name: "AliceBlue",
+  },
+  {
+    name: "AntiqueWhite",
+  },
+  {
+    name: "Aqua",
+  },
+  {
+    name: "Aquamarine",
+  },
+  {
+    name: "Azure",
+  },
+  {
+    name: "Beige",
+  },
+  {
+    name: "Bisque",
+  },
+  {
+    name: "Black",
+  },
+  {
+    name: "BlanchedAlmond",
+  },
+  {
+    name: "Blue",
+  },
+  {
+    name: "BlueViolet",
+  },
+  {
+    name: "Brown",
+  },
+  {
+    name: "BurlyWood",
+  },
+  {
+    name: "CadetBlue",
+  },
+  {
+    name: "Chartreuse",
+  },
+  {
+    name: "Chocolate",
+  },
+  {
+    name: "Coral",
+  },
+  {
+    name: "CornflowerBlue",
+  },
+  {
+    name: "Cornsilk",
+  },
+  {
+    name: "Crimson",
+  },
+  {
+    name: "Cyan",
+  },
+  {
+    name: "DarkBlue",
+  },
+  {
+    name: "DarkCyan",
+  },
+  {
+    name: "DarkGoldenRod",
+  },
+  {
+    name: "DarkGray",
+  },
+  {
+    name: "DarkGrey",
+  },
+  {
+    name: "DarkGreen",
+  },
+  {
+    name: "DarkKhaki",
+  },
+  {
+    name: "DarkMagenta",
+  },
+  {
+    name: "DarkOliveGreen",
+  },
+  {
+    name: "DarkOrange",
+  },
+  {
+    name: "DarkOrchid",
+  },
+  {
+    name: "DarkRed",
+  },
+  {
+    name: "DarkSalmon",
+  },
+  {
+    name: "DarkSeaGreen",
+  },
+  {
+    name: "DarkSlateBlue",
+  },
+  {
+    name: "DarkSlateGray",
+  },
+  {
+    name: "DarkSlateGrey",
+  },
+  {
+    name: "DarkTurquoise",
+  },
+  {
+    name: "DarkViolet",
+  },
+  {
+    name: "DeepPink",
+  },
+  {
+    name: "DeepSkyBlue",
+  },
+  {
+    name: "DimGray",
+  },
+  {
+    name: "DimGrey",
+  },
+  {
+    name: "DodgerBlue",
+  },
+  {
+    name: "FireBrick",
+  },
+  {
+    name: "FloralWhite",
+  },
+  {
+    name: "ForestGreen",
+  },
+  {
+    name: "Fuchsia",
+  },
+  {
+    name: "Gainsboro",
+  },
+  {
+    name: "GhostWhite",
+  },
+  {
+    name: "Gold",
+  },
+  {
+    name: "GoldenRod",
+  },
+  {
+    name: "Gray",
+  },
+  {
+    name: "Grey",
+  },
+  {
+    name: "Green",
+  },
+  {
+    name: "GreenYellow",
+  },
+  {
+    name: "HoneyDew",
+  },
+  {
+    name: "HotPink",
+  },
+  {
+    name: "IndianRed ",
+  },
+  {
+    name: "Indigo  ",
+  },
+  {
+    name: "Ivory",
+  },
+  {
+    name: "Khaki",
+  },
+  {
+    name: "Lavender",
+  },
+  {
+    name: "LavenderBlush",
+  },
+  {
+    name: "LawnGreen",
+  },
+  {
+    name: "LemonChiffon",
+  },
+  {
+    name: "LightBlue",
+  },
+  {
+    name: "LightCoral",
+  },
+  {
+    name: "LightCyan",
+  },
+  {
+    name: "LightGoldenRodYellow",
+  },
+  {
+    name: "LightGray",
+  },
+  {
+    name: "LightGrey",
+  },
+  {
+    name: "LightGreen",
+  },
+  {
+    name: "LightPink",
+  },
+  {
+    name: "LightSalmon",
+  },
+  {
+    name: "LightSeaGreen",
+  },
+  {
+    name: "LightSkyBlue",
+  },
+  {
+    name: "LightSlateGray",
+  },
+  {
+    name: "LightSlateGrey",
+  },
+  {
+    name: "LightSteelBlue",
+  },
+  {
+    name: "LightYellow",
+  },
+  {
+    name: "Lime",
+  },
+  {
+    name: "LimeGreen",
+  },
+  {
+    name: "Linen",
+  },
+  {
+    name: "Magenta",
+  },
+  {
+    name: "Maroon",
+  },
+  {
+    name: "MediumAquaMarine",
+  },
+  {
+    name: "MediumBlue",
+  },
+  {
+    name: "MediumOrchid",
+  },
+  {
+    name: "MediumPurple",
+  },
+  {
+    name: "MediumSeaGreen",
+  },
+  {
+    name: "MediumSlateBlue",
+  },
+  {
+    name: "MediumSpringGreen",
+  },
+  {
+    name: "MediumTurquoise",
+  },
+  {
+    name: "MediumVioletRed",
+  },
+  {
+    name: "MidnightBlue",
+  },
+  {
+    name: "MintCream",
+  },
+  {
+    name: "MistyRose",
+  },
+  {
+    name: "Moccasin",
+  },
+  {
+    name: "NavajoWhite",
+  },
+  {
+    name: "Navy",
+  },
+  {
+    name: "OldLace",
+  },
+  {
+    name: "Olive",
+  },
+  {
+    name: "OliveDrab",
+  },
+  {
+    name: "Orange",
+  },
+  {
+    name: "OrangeRed",
+  },
+  {
+    name: "Orchid",
+  },
+  {
+    name: "PaleGoldenRod",
+  },
+  {
+    name: "PaleGreen",
+  },
+  {
+    name: "PaleTurquoise",
+  },
+  {
+    name: "PaleVioletRed",
+  },
+  {
+    name: "PapayaWhip",
+  },
+  {
+    name: "PeachPuff",
+  },
+  {
+    name: "Peru",
+  },
+  {
+    name: "Pink",
+  },
+  {
+    name: "Plum",
+  },
+  {
+    name: "PowderBlue",
+  },
+  {
+    name: "Purple",
+  },
+  {
+    name: "RebeccaPurple",
+  },
+  {
+    name: "Red",
+  },
+  {
+    name: "RosyBrown",
+  },
+  {
+    name: "RoyalBlue",
+  },
+  {
+    name: "SaddleBrown",
+  },
+  {
+    name: "Salmon",
+  },
+  {
+    name: "SandyBrown",
+  },
+  {
+    name: "SeaGreen",
+  },
+  {
+    name: "SeaShell",
+  },
+  {
+    name: "Sienna",
+  },
+  {
+    name: "Silver",
+  },
+  {
+    name: "SkyBlue",
+  },
+  {
+    name: "SlateBlue",
+  },
+  {
+    name: "SlateGray",
+  },
+  {
+    name: "SlateGrey",
+  },
+  {
+    name: "Snow",
+  },
+  {
+    name: "SpringGreen",
+  },
+  {
+    name: "SteelBlue",
+  },
+  {
+    name: "Tan",
+  },
+  {
+    name: "Teal",
+  },
+  {
+    name: "Thistle",
+  },
+  {
+    name: "Tomato",
+  },
+  {
+    name: "Turquoise",
+  },
+  {
+    name: "Violet",
+  },
+  {
+    name: "Wheat",
+  },
+  {
+    name: "White",
+  },
+  {
+    name: "WhiteSmoke",
+  },
+  {
+    name: "Yellow",
+  },
+];
