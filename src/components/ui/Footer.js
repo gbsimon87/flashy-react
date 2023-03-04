03a39acc0a912b88c7ae8ede629fd41ea43d5354
Simon Cordova <simon.cordova@creative-cx.com>
2023-03-04 18:33:32 +0000

    change background image

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="footer">
-      <p>Flashy Inc. &copy; 2021</p>
+      <p>Flash Forward Inc. &copy; {new Date().getFullYear()}</p>
     </footer>
   )
 }
